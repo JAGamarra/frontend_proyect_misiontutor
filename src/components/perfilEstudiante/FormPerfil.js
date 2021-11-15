40ea3e85f92e63585be94d95fbca8ab72382a570
jcamilobm <jcbetanm@gmail.com>
2021-11-15 09:59:37 -0500

    perfil estudiante

---
+++ src/components/perfilEstudiante/FormPerfil.js
@@ -0,0 +1,64 @@
+import { Form,Row,Col,Button} from 'react-bootstrap';
+
+const FormPerfilEstudiante = () => {
+  return (
+    <>
+      <Form>
+    
+        <Row className="mb-3">
+          <Form.Group as={Col} >
+            <Form.Label>Nombre</Form.Label>
+            <Form.Control type="text" placeholder="nombre" />
+          </Form.Group>
+
+          <Form.Group as={Col} >
+            <Form.Label>Apellido</Form.Label>
+            <Form.Control type="apellido" placeholder="apellido" />
+          </Form.Group>
+        </Row>
+
+        <Row className="mb-3">
+          <Form.Group as={Col} >
+            <Form.Label>Correo electrónico</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group as={Col} >
+            <Form.Label>Dirección</Form.Label>
+            <Form.Control type="text" placeholder="Tu dirección" />
+          </Form.Group>
+        </Row>
+
+        <Row className="mb-3">
+          <Form.Group as={Col} >
+            <Form.Label>Departamento</Form.Label>
+            <Form.Select defaultValue="Choose...">
+              <option>Escoge...</option>
+              <option>...</option>
+            </Form.Select>
+          </Form.Group>
+
+          <Form.Group as={Col} >
+            <Form.Label>Ciudad</Form.Label>
+            <Form.Select defaultValue="Choose...">
+              <option>Escoge...</option>
+              <option>...</option>
+            </Form.Select>
+          </Form.Group>
+
+          <Form.Group as={Col} >
+            <Form.Label>Edad</Form.Label>
+            <Form.Control type='number'/>
+          </Form.Group>
+        </Row>
+
+        <Button className="mt-3" size="lg" variant="primary" type="submit">
+          Guardar
+        </Button>
+      </Form>
+
+    </>
+  );
+};
+
+export default FormPerfilEstudiante;
---
+++ src/components/perfilEstudiante/PerfilEstudiante.js
@@ -0,0 +1,44 @@
+//Componentes de Bootstrap
+import { Container, Row, Col, Button ,Badge,Image} from "react-bootstrap";
+import FormPerfilEstudiante from "../perfilEstudiante/FormPerfil";
+
+const PerfilEstudiante = () => {
+  return (
+    <Container style={{ height: '79vh'}}>
+
+      {/* imagen */}
+      <Row>
+        <Col>
+             <Image src="https://cdn.pixabay.com/photo/2015/11/05/08/21/geometry-1023846_960_720.jpg" thumbnail  style={{ height: '130px',width:'500px'}} fluid alt="estudiante"/>
+        </Col>
+      </Row>
+
+      {/* botón */}
+      <Row className="d-flex justify-content-center ">
+        <Col xs={10} md={8} >
+          <div className="d-grid gap-3 mt-5 mb-5">
+            <Button variant="primary" size="lg">
+              Buscar profesor
+            </Button>
+          </div>
+        </Col>
+      </Row>
+
+      {/* formulario */}
+      <Row className="d-flex justify-content-center">
+        <Col xs={12} className="mb-5">
+          <Badge bg="secondary"><h2>Perfil estudiante</h2></Badge>   
+        </Col>
+        <Col xs={12} lg={8} >
+          
+          <FormPerfilEstudiante />
+         
+        </Col>
+      
+      </Row>
+
+    </Container>
+  );
+};
+
+export default PerfilEstudiante;
